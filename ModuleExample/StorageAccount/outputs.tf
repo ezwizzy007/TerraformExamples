463469f748ed1a8977afd20b2b5473c85f85c3d3
dadurg <dadurg@pvh.com>
2023-08-28 21:08:29 -0400

    add rg tf files to main

--- ModuleExample/StorageAccount/outputs.tf
+++ ModuleExample/StorageAccount/outputs.tf
@@ -1,3 +1,8 @@
 output "stg_act_name_out" {
   value = resource.azurerm_storage_account.example.name
 }
+
+output "stgAccessTier_out" {
+  value = resource.azurerm_storage_account.example.access_tier
+  description = "access tier"
+}
--- ModuleExample/outputs.tf
+++ ModuleExample/outputs.tf
@@ -1,7 +1,14 @@
+
 output "StgActName" {
   value = module.StorageAccount.stg_act_name_out
 }
 
 output "RgName" {
   value = module.ResourceGroup.rg_name_out
+}
+
+
+output "stgAccessTier" {
+  value = module.StorageAccount.stgAccessTier_out
+  description = "access tier"
 }
